30fb1142249c14ff98508b0fc8ef6148c58e8c1a
Renato Dourado <64652687+renatodourado@users.noreply.github.com>
2020-07-13 16:00:41 -0300

    Add files via upload

---
+++ pizzas.js
@@ -0,0 +1,11 @@
+let pizzaJson = [
+    { id: 1, name: 'Mussarela', img: 'images/mussarela.jpg', price: 20.19, sizes: ['100g', '530g', '860g'], description: 'A Pizza de Mussarela é coberta com molho de tomate, queijo tipo mussarela, azeitonas, orégano e massa com fermentação natural, oferece mais sabor e qualidade à sua mesa.' },
+    { id: 2, name: 'Calabresa', img: 'images/pizza2.png', price: 18.00, sizes: ['320g', '530g', '860g'], description: 'Uma deliciosa combinação de Linguiça Calabresa, rodelas de cebolas frescas, azeitonas pretas, mussarela, polpa de tomate, orégano e massa especial garante um sabor único à Pizza de Calabresa' },
+    { id: 3, name: 'Quatro Queijos', img: 'images/4-Q.jpg', price: 17.45, sizes: ['320g', '530g', '860g'], description: 'Combinamos os queijos Provolone, Mussarela, Parmesão e Catupiry, com um delicioso molho de tomate.' },
+    { id: 4, name: 'Americana', img: 'images/pizza4.png', price: 19.77, sizes: ['320g', '530g', '860g'], description: 'Molho de tomate fresco, presunto, ervilha, mussarela, palmito e ovos.' },
+    { id: 5, name: 'Sorvete', img: 'images/sorvete.jpg', price: 21.43, sizes: ['320g', '530g', '860g'], description: 'Pizza com bolas de Sorvete de Morango, Chocolate, Creme e Doce de Leite  ' },
+    { id: 6, name: 'Moda da Casa', img: 'images/pizza6.png', price: 18.55, sizes: ['320g', '530g', '860g'], description: '5 ingredientes escolhidos pelo cliente' },
+    { id: 7, name: 'Chocolate', img: 'images/pizza7.png', price: 22.36, sizes: ['320g', '530g', '860g'], description: 'Pizza de Chocolate com confeitos ou banana ' }
+];
+
+console.log(pizzaJson);
